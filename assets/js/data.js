c5cce773e9ea600f43198cf739366072dab85ae2
Mariano Mercado <maomercado12@gmail.com>
2024-06-02 22:39:42 -0300

    section projects

---
+++ assets/js/data.js
@@ -0,0 +1,51 @@
+const projectsData = [
+  {
+    id: 1,
+    title: "API de Pókemon.",
+    description: "Primera práctica con API.",
+    img: "assets/img/projects/pokemon.png",
+    category: "API",
+    gitHub: "https://github.com/MarianoMercado/PokeApi-js",
+    sitio: "https://poke-api-ihw2b5pti-marianomercado.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Ramen Autos",
+    description:
+      "Mi primera practica con JS, incluyendo logica con carrito y stock",
+    img: "assets/img/projects/car.jpg",
+    category: "JS",
+    gitHub: "https://github.com/MarianoMercado/IntegradorJs",
+    sitio: "https://integrador-d2kkih96k-marianomercado.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Ramen Autos",
+    description:
+      "Mi primera practica con JS, incluyendo logica con carrito y stock",
+    img: "assets/img/projects/car.jpg",
+    category: "JS",
+    gitHub: "https://github.com/MarianoMercado/IntegradorJs",
+    sitio: "https://integrador-d2kkih96k-marianomercado.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Ramen Autos",
+    description:
+      "Mi primera practica con JS, incluyendo logica con carrito y stock",
+    img: "assets/img/projects/car.jpg",
+    category: "JS",
+    gitHub: "https://github.com/MarianoMercado/IntegradorJs",
+    sitio: "https://integrador-d2kkih96k-marianomercado.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Ramen Autos",
+    description:
+      "Mi primera practica con JS, incluyendo logica con carrito y stock",
+    img: "assets/img/projects/car.jpg",
+    category: "JS",
+    gitHub: "https://github.com/MarianoMercado/IntegradorJs",
+    sitio: "https://integrador-d2kkih96k-marianomercado.vercel.app/",
+  },
+];
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,60 @@ const MenuNavbar = document.querySelector(".toggle-menu");
 const navbarlist = document.querySelector(".navbar-list");
 const overlay = document.querySelector(".overlay");
 
+const projectsContainer = document.querySelector(".projects-container");
+
+//cerrar al hacer click fuera del menu
 const closeOnOverlayClick = () => {
   navbarlist.classList.remove("open-Navbar");
-
   overlay.classList.toggle("show-overlay");
 };
+
+//cerrar  menu
 const closeOnClick = (e) => {
-  debugger;
   if (!e.target.classList.contains("navbar-link")) {
     return;
   }
   navbarlist.classList.remove("open-Navbar");
   overlay.classList.remove("show-overlay");
 };
+
 /*navbar* */
 const toggleMenu = () => {
   navbarlist.classList.toggle("open-Navbar");
   overlay.classList.toggle("show-overlay");
 };
 
+//crear cards
+const createProjetsCard = (projet) => {
+  const { id, title, description, img, category, gitHub, sitio } = projet;
+
+  return `   
+          <div class="projects-card">
+            <img src="${img}" alt="${(title, category)}" >
+              <div class="projects-card-description">
+                <strong>${title}</strong>
+                <span>${description}</span>
+              </div>
+              <div class="projects-card-info">
+                <a href="${gitHub}" target="_blank">
+                   github <i class="fa-brands fa-github"></i>
+                </a>
+                 <a href="${sitio}" target="_blank">
+                  Sitio <i class="fa-solid fa-globe"></i>
+                </a> 
+              </div>
+          </div>       
+`;
+};
+
+//render projects
+const renderProjects = () => {
+  debugger;
+  projectsContainer.innerHTML = projectsData.map(createProjetsCard).join("");
+};
+
 const init = () => {
+  renderProjects();
   MenuNavbar.addEventListener("click", toggleMenu);
   overlay.addEventListener("click", closeOnOverlayClick);
   navbarlist.addEventListener("click", closeOnClick);
